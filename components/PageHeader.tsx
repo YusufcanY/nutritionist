34eb6d761d7c252c86d0ef95965df5e2e36c8efd
Yusufcan Yilmaz <playders03@gmail.com>
2025-10-24 20:04:18 +0300

    feat: added transitions to page header comp

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Logo from '@/components/icons/Logo';
 import { cn } from '@/lib/utils';
-
+import * as motion from 'motion/react-client';
 export default function PageHeader({
   title,
   description,
@@ -20,18 +20,30 @@ export default function PageHeader({
         )}
       >
         <div className='relative z-10 size-[62px] laptop:size-[68px] desktop:size-24'>
-          <div className='rounded-[10px] border-4 border-white bg-gradient-to-b from-green-70 to-white p-3.5 desktop:rounded-xl desktop:p-5'>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3 }}
+            className='rounded-[10px] border-4 border-white bg-gradient-to-b from-green-70 to-white p-3.5 desktop:rounded-xl desktop:p-5'
+          >
             <Logo className='h-full w-full text-[#1A3129]' />
-          </div>
+          </motion.div>
         </div>
-        <div className='space-y-1.5 laptop:space-y-2.5 desktop:space-y-5'>
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3 }}
+          className='space-y-1.5 laptop:space-y-2.5 desktop:space-y-5'
+        >
           <h1 className='text-center text-3xl font-semibold text-grey-15 laptop:text-4xl desktop:text-5xl'>
             {title}
           </h1>
           <p className='text-center text-sm font-medium text-grey-20 laptop:text-base desktop:text-lg'>
             {description}
           </p>
-        </div>
+        </motion.div>
         <Image
           src='/abstract-3.svg'
           alt='abstract'
