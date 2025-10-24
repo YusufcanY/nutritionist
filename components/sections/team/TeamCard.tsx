d4a5f8fc0d8c99c9c556d2d4c3bf29a845314b17
Yusufcan Yilmaz <playders03@gmail.com>
2025-10-24 19:20:49 +0300

    feat: improved ui of teamcard comp

--- components/sections/team/TeamCard.tsx
+++ components/sections/team/TeamCard.tsx
@@ -1,3 +1,5 @@
+import { DotPattern } from '@/components/ui/DotPattern';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 interface TeamCardProps {
@@ -8,15 +10,23 @@ interface TeamCardProps {
 export default function TeamCard({ name, title, image }: TeamCardProps) {
   return (
     <div className='relative rounded-lg bg-green-70 desktop:rounded-xl'>
-      <Image
-        src={image}
-        alt={name}
-        width={0}
-        height={0}
-        sizes='100vw'
-        className='h-auto max-h-[305px] w-full object-cover object-top'
-      />
-      <div className='absolute bottom-0 left-0 w-3/4 translate-y-1/2 space-y-0.5 rounded-tr-[20px] bg-green-95 p-5 desktop:space-y-1 desktop:p-8'>
+      <div className='relative'>
+        <Image
+          src={image}
+          alt={name}
+          width={0}
+          height={0}
+          sizes='100vw'
+          loading='eager'
+          className='relative z-10 h-auto max-h-[305px] w-full object-cover object-top'
+        />
+        <DotPattern
+          className={cn(
+            'absolute top-0 left-0 [mask-image:radial-gradient(175px_circle_at_center,white,transparent)] text-dark-green-60'
+          )}
+        />
+      </div>
+      <div className='absolute bottom-0 left-0 z-20 w-3/4 translate-y-1/2 space-y-0.5 rounded-tr-[20px] bg-green-95 p-5 desktop:space-y-1 desktop:p-8'>
         <h4 className='text-xl font-semibold text-grey-15 desktop:text-2xl'>
           {name}
         </h4>
