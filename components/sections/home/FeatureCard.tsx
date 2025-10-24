da88f3e8d6d0cf430997833d044f2042643ae52e
Yusufcan Yilmaz <playders03@gmail.com>
2025-10-24 22:37:53 +0300

    feat: process page added

--- components/sections/home/FeatureCard.tsx
+++ components/sections/home/FeatureCard.tsx
@@ -5,6 +5,7 @@ interface FeatureCardProps {
   icon: ReactNode;
   title: string;
   description: string;
+  subtitle?: string;
   className?: string;
 }
 
@@ -12,26 +13,34 @@ export default function FeatureCard({
   icon,
   title,
   description,
+  subtitle,
   className,
 }: FeatureCardProps) {
   return (
-    <div
-      className={cn(
-        'h-full rounded-lg border border-green-85 bg-green-95 p-6 laptop:p-8 desktop:rounded-xl desktop:p-10',
-        className
+    <div className='flex h-full flex-col'>
+      {subtitle && (
+        <span className='translate-y-1/3 text-7xl font-bold text-dark-green-40 laptop:text-8xl desktop:text-9xl'>
+          {subtitle}
+        </span>
       )}
-    >
-      <div className='flex items-center gap-4'>
-        <div className='mb-4 flex size-12 items-center justify-center rounded-lg bg-green-70 text-grey-15 laptop:size-14 desktop:size-16'>
-          {icon}
+      <div
+        className={cn(
+          'relative z-10 flex-grow rounded-lg border border-green-85 bg-green-95 p-6 laptop:p-8 desktop:rounded-xl desktop:p-10',
+          className
+        )}
+      >
+        <div className='flex items-center gap-4'>
+          <div className='mb-4 flex size-12 items-center justify-center rounded-lg bg-green-70 text-grey-15 laptop:size-14 desktop:size-16'>
+            {icon}
+          </div>
+          <h3 className='mb-3 text-lg font-semibold text-grey-15 laptop:text-xl desktop:text-2xl'>
+            {title}
+          </h3>
         </div>
-        <h3 className='mb-3 text-lg font-semibold text-grey-15 laptop:text-xl desktop:text-2xl'>
-          {title}
-        </h3>
+        <p className='text-sm font-medium text-grey-20 laptop:text-base desktop:text-lg'>
+          {description}
+        </p>
       </div>
-      <p className='text-sm font-medium text-grey-20 laptop:text-base desktop:text-lg'>
-        {description}
-      </p>
     </div>
   );
 }
